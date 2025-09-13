15cde95ac68a5b5cd33c5a2ac179497ea564cd03
nenadgolubovic <nenad.golubovic999@gmail.com>
2025-09-13 13:28:39 +0200

    Initial commit

---
+++ document-review/src/main/java/rs/nenadgolubovic/document_review/DocumentReviewSoftwareApplication.java
@@ -0,0 +1,13 @@
+package rs.nenadgolubovic.document_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentReviewSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentReviewSoftwareApplication.class, args);
+	}
+
+}
---
+++ document-review/src/test/java/rs/nenadgolubovic/document_review/DocumentReviewSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.nenadgolubovic.document_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentReviewSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
