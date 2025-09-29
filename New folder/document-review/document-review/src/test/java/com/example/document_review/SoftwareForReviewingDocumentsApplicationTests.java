2d6284651bf9aae07f6bdb1b59f22f8310fc53f7
nenadgolubovic <nenad.golubovic999@gmail.com>
2025-09-29 23:36:56 +0200

    Update SoftwareForReviewingDocumentsApplicationTests.java

--- New folder/document-review/document-review/src/test/java/com/example/document_review/SoftwareForReviewingDocumentsApplicationTests.java
+++ New folder/document-review/document-review/src/test/java/com/example/document_review/SoftwareForReviewingDocumentsApplicationTests.java
@@ -2,10 +2,10 @@ package com.example.document_review;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(locations = "file:src/test/application-test.properties")
-
 class SoftwareForReviewingDocumentsApplicationTests {
 
 	@Test
