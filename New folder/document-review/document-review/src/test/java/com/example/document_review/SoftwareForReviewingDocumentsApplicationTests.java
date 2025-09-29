599df5afc45b55f8e0409bd777759ed602004360
nenadgolubovic <nenad.golubovic999@gmail.com>
2025-09-29 23:23:13 +0200

    Update SoftwareForReviewingDocumentsApplicationTests.java

--- New folder/document-review/document-review/src/test/java/com/example/document_review/SoftwareForReviewingDocumentsApplicationTests.java
+++ New folder/document-review/document-review/src/test/java/com/example/document_review/SoftwareForReviewingDocumentsApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2) // koristi H2 u testu
+
 class SoftwareForReviewingDocumentsApplicationTests {
 
 	@Test
