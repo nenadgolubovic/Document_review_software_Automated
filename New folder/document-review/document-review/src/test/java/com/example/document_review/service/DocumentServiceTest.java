6b598d04c31348f11f7bf8805a71e259c16f5771
nenadgolubovic <nenad.golubovic999@gmail.com>
2025-09-30 14:06:18 +0200

    Finished DocumentServiceTest

--- New folder/document-review/document-review/src/test/java/com/example/document_review/service/DocumentServiceTest.java
+++ New folder/document-review/document-review/src/test/java/com/example/document_review/service/DocumentServiceTest.java
@@ -2,54 +2,236 @@ package com.example.document_review.service;
 
 
 import com.example.document_review.dto.DocumentDto;
+import com.example.document_review.dto.FanBladeDto;
+import com.example.document_review.dto.PartDto;
 import com.example.document_review.entity.Document;
 import com.example.document_review.entity.Enums.PartType;
 import com.example.document_review.entity.FanBlade;
 import com.example.document_review.mapper.impl.DocumentMapper;
+import com.example.document_review.mapper.impl.PartMapper;
 import com.example.document_review.repository.impl.DocumentRepository;
 import com.example.document_review.service.impl.DocumentServiceImpl;
+import com.example.document_review.service.impl.PartServiceImpl;
+import com.example.document_review.validator.impl.DocumentFileValidator;
+import com.example.document_review.validator.impl.DocumentSaveValidator;
+import jakarta.mail.Part;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.core.io.ResourceLoader;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class DocumentServiceTest {
 
 
     @Mock
-    DocumentRepository documentRepository;
+    private PartServiceImpl partServiceImpl;
 
     @Mock
-    DocumentMapper documentMapper;
+    private PartMapper partMapper;
+
+    @Mock
+    private DocumentRepository documentRepository;
+
+    @Mock
+    private DocumentSaveValidator documentSaveValidator;
+
+    @Mock
+    private DocumentFileValidator documentFileValidator;
+
+    @Mock
+    private DocumentMapper documentMapper;
 
     @InjectMocks
-    DocumentServiceImpl documentServiceImpl;
+    private DocumentServiceImpl documentServiceImpl; // koristi mokove
+
 
 
     @Test
-    public void DocumentService_UploadDocument(){
+    void uploadDocument_ShouldSaveDocument() throws IOException {
 
+        FanBlade fanBlade = FanBlade.builder()
+                .partNumber("PartNumberTest")
+                .description("DescriptionTest")
+                .serialNumber("SerialNumberTest")
+                .type(PartType.FanBlade)
+                .cyclesSinceNew("CycleSinceNewTest")
+                .timeSinceNew("TimeSinceNewTest")
+                .momentWeight("MomentWeightTest")
+                .build();
+        FanBladeDto fanBladeDto = FanBladeDto.builder()
+                .partNumber("PartNumberTest")
+                .description("DescriptionTest")
+                .serialNumber("SerialNumberTest")
+                .type(PartType.FanBlade)
+                .cyclesSinceNew("CycleSinceNewTest")
+                .timeSinceNew("TimeSinceNewTest")
+                .momentWeight("MomentWeightTest")
+                .build();
+
+        Document document1 = Document.builder()
+                .documentName("DocumentNameTest")
+                .partId(1)
+                .documentRoute("DocumentRouteTest.pdf")
+                .documentDate(LocalDate.now())
+                .build();
+
+        DocumentDto documentDto = DocumentDto.builder()
+                .documentName("DocumentNameTest")
+                .partId(1)
+                .documentRoute("DocumentRouteTest.pdf")
+                .documentDate(LocalDate.now())
+                .build();
+
+        MultipartFile fakeFile = new MockMultipartFile(
+                "document",
+                "test.pdf",
+                "application/pdf",
+                "Dummy PDF content".getBytes()
+        );
+
+
+        when(partServiceImpl.getById(documentDto.getPartId())).thenReturn(fanBladeDto);
+        when(partMapper.toEntity(fanBladeDto)).thenReturn(mock(FanBlade.class));
+        when(documentMapper.toEntity(documentDto)).thenReturn(mock(Document.class));
+
+
+        documentServiceImpl.uploadDocument(fakeFile, documentDto);
+
+
+        assertNotNull(documentDto.getDocumentName());
+        assertNotNull(documentDto.getDocumentRoute());
+        assertEquals(LocalDate.now(), documentDto.getDocumentDate());
+
+
+        verify(partServiceImpl, times(1)).getById(documentDto.getPartId());
+        verify(partMapper, times(1)).toEntity(fanBladeDto);
+        verify(documentSaveValidator, times(1)).validate(documentDto);
+        verify(documentFileValidator, times(1)).validate(fakeFile);
+        verify(documentMapper, times(1)).toEntity(documentDto);
+        verify(documentRepository, times(1)).save(any(Document.class));
+
+    }
+
+    @Test
+    void uploadDocument_ShouldThrowException_WhenFileIsEmpty() {
+        MultipartFile multipartFile = mock(MultipartFile.class);
+        DocumentDto documentDto = new DocumentDto();
+        documentDto.setPartId(1);
+
+        when(multipartFile.isEmpty()).thenReturn(true);
+
+        // Act & Assert
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                documentServiceImpl.uploadDocument(multipartFile, documentDto)
+        );
+
+        assertEquals("File name is invalid", exception.getMessage());
     }
 
     @Test
     public void DocumentService_GetById_DocumentDtos(){
+        FanBlade fanBlade = FanBlade.builder()
+                .partNumber("PartNumberTest")
+                .description("DescriptionTest")
+                .serialNumber("SerialNumberTest")
+                .type(PartType.FanBlade)
+                .cyclesSinceNew("CycleSinceNewTest")
+                .timeSinceNew("TimeSinceNewTest")
+                .momentWeight("MomentWeightTest")
+                .build();
+
+
+        Document document1 = Document.builder()
+                .documentName("DocumentNameTest")
+                .partId(fanBlade.getPartId())
+                .documentRoute("DocumentRouteTest.pdf")
+                .documentDate(LocalDate.now())
+                .build();
+
+        Document document2 = Document.builder()
+                .documentName("DocumentNameTest2")
+                .partId(fanBlade.getPartId())
+                .documentRoute("DocumentRouteTest2.pdf")
+                .documentDate(LocalDate.now())
+                .build();
+        DocumentDto documentDto1 = DocumentDto.builder()
+                .documentName("DocumentNameTest1")
+                .partId(fanBlade.getPartId())
+                .documentRoute("DocumentRouteTest1.pdf")
+                .documentDate(LocalDate.now())
+                .build();
+
+        DocumentDto documentDto2 = DocumentDto.builder()
+                .documentName("DocumentNameTest2")
+                .partId(fanBlade.getPartId())
+                .documentRoute("DocumentRouteTest2.pdf")
+                .documentDate(LocalDate.now())
+                .build();
+
+
+        List<Document> documents = Arrays.asList(document1, document2);
+
+        when(documentRepository.getByPartId(1)).thenReturn(documents);
+        when(documentMapper.toDto(document1)).thenReturn(documentDto1);
+        when(documentMapper.toDto(document2)).thenReturn(documentDto2);
+
+
+        List<DocumentDto> result = documentServiceImpl.getByPartId(1);
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals(documentDto1, result.get(0));
+        assertEquals(documentDto2, result.get(1));
+        verify(documentRepository, times(1)).getByPartId(1);
+        verify(documentMapper, times(1)).toDto(document1);
+        verify(documentMapper, times(1)).toDto(document2);
 
     }
 
     @Test
-    public void DocumentService_GetDocumentsByName_Resource(){
+    public void DocumentService_GetDocumentsByName_Resource() throws IOException {
+        String filename = "test.pdf";
 
+        Path path = Paths.get("C:/Users/Nikola/.../document/" + filename);
+        Resource resource = new UrlResource(path.toUri());
+
+        Resource result = documentServiceImpl.getDocumentByName(filename);
+
+        if (!resource.exists() || !resource.isReadable()) {
+            assertNull(result);
+        } else {
+            assertNotNull(result);
+            assertEquals(resource.getURI(), result.getURI());
+        }
+    }
+    @Test
+    void testGetDocumentByName_FileDoesNotExist() throws IOException {
+        String filename = "nonexistent.pdf";
+
+        Resource result = documentServiceImpl.getDocumentByName(filename);
+
+        assertNull(result);
     }
 
     @Test
