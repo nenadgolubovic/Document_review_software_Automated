c5789a49741a78e59802ac6249ee2a0ddd4d208c
nenadgolubovic <nenad.golubovic999@gmail.com>
2025-09-30 17:54:50 +0200

    Finished DocumentControllerTest

--- New folder/document-review/document-review/src/test/java/com/example/document_review/controller/BasicPartControllerTest.java
+++ New folder/document-review/document-review/src/test/java/com/example/document_review/controller/BasicPartControllerTest.java
@@ -78,6 +78,9 @@ public class BasicPartControllerTest {
 
         verify(basicPartServiceImpl, times(1)).save(refEq(basicPartDto1));
 
-
     }
+
+
+
+
 }
--- New folder/document-review/document-review/src/test/java/com/example/document_review/controller/DocumentControllerTest.java
+++ New folder/document-review/document-review/src/test/java/com/example/document_review/controller/DocumentControllerTest.java
@@ -8,6 +8,7 @@ import com.example.document_review.entity.Document;
 import com.example.document_review.entity.Enums.PartType;
 import com.example.document_review.service.impl.BasicPartServiceImpl;
 import com.example.document_review.service.impl.DocumentServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -24,15 +27,17 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.refEq;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = DocumentController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -63,10 +68,17 @@ public class DocumentControllerTest {
                 .timeSinceNew("TimeSinceNewTest")
                 .momentWeight("MomentWeightTest")
                 .build();
-        DocumentDto documentDto = DocumentDto.builder()
+
+        documentDto1 = DocumentDto.builder()
                 .documentName("DocumentNameTest")
                 .partId(1)
                 .documentRoute("DocumentRouteTest.pdf")
+                .documentDate(LocalDate.now())
+                .build();
+        documentDto2 = DocumentDto.builder()
+                .documentName("DocumentNameTest2")
+                .partId(1)
+                .documentRoute("DocumentRouteTest2.pdf")
                 .documentDate(LocalDate.now())
                 .build();
     }
@@ -102,6 +114,111 @@ public class DocumentControllerTest {
                 .andExpect(content().string(containsString("File uploaded successfully: test-document.pdf")));
 
         verify(documentServiceImpl, times(1)).uploadDocument(any(MultipartFile.class), any(DocumentDto.class));
+    }
+
+    @Test
+    public void DocumentController_getAll_DocumentsDto() throws Exception {
+        List<DocumentDto> documents = Arrays.asList(documentDto1, documentDto2);
+
+        when(documentServiceImpl.findAllDocuments()).thenReturn(documents);
+
+        mockMvc.perform(get("/document/getAll")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(documents)));
+    }
+
+    @Test
+    public void DocumentController_GetDocument_Resource() throws Exception {
+        String filename = "test.pdf";
+        Resource resource = new ByteArrayResource("dummy pdf content".getBytes());
+
+        when(documentServiceImpl.getDocumentByName(filename)).thenReturn(resource);
+
+        mockMvc.perform(get("/document/{filename}", filename))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_PDF));
+    }
+
+    @Test
+    public void DocumentController_GetDocument_ShouldReturnBadRequest() throws Exception {
+        String filename = "missing.pdf";
+
+        when(documentServiceImpl.getDocumentByName(filename)).thenReturn(null);
+
+        mockMvc.perform(get("/document/{filename}", filename))
+                .andExpect(status().isBadRequest()); // jer Spring vraća 400
+    }
+
+    @Test
+    public void DocumentController_GetByPartId_DocumentsDto() throws Exception {
+
+        Integer partId = 1;
+        List<DocumentDto> documents = Arrays.asList(documentDto1, documentDto2);
+
+        when(documentServiceImpl.getByPartId(partId)).thenReturn(documents);
+
+        mockMvc.perform(get("/get/part/{id}", partId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].documentName").value("DocumentNameTest"))
+                .andExpect(jsonPath("$[0].partId").value(1))
+                .andExpect(jsonPath("$[0].documentRoute").value("DocumentRouteTest.pdf"))
+                .andExpect(jsonPath("$[1].documentName").value("DocumentNameTest2"))
+                .andExpect(jsonPath("$[1].documentRoute").value("DocumentRouteTest2.pdf"));
+
+        verify(documentServiceImpl, times(1)).getByPartId(partId);
+    }
+    @Test
+    public void DocumentController_GetById_DocumentsDtoShouldReturnDocument() throws Exception {
+        Integer documentId = 1;
+
+        when(documentServiceImpl.findById(documentId)).thenReturn(documentDto1);
+
+        mockMvc.perform(get("/document/get/{id}", documentId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.documentName").value("DocumentNameTest"))
+                .andExpect(jsonPath("$.partId").value(1))
+                .andExpect(jsonPath("$.documentRoute").value("DocumentRouteTest.pdf"));
+
+        verify(documentServiceImpl, times(1)).findById(documentId);
+    }
+    @Test
+    public void DocumentController_GetById_DocumentsDtoShouldReturnNotFound() throws Exception {
+        Integer documentId = 99;
+
+        when(documentServiceImpl.findById(documentId)).thenReturn(null);
+
+        mockMvc.perform(get("/document/get/{id}", documentId))
+                .andExpect(status().isNotFound());
+
+        verify(documentServiceImpl, times(1)).findById(documentId);
+    }
+    @Test
+    public void DocumentController_DeleteById_ShouldReturnOk() throws Exception {
+        Integer documentId = 1;
+
+        when(documentServiceImpl.deleteById(documentId)).thenReturn(documentDto1);
+
+        mockMvc.perform(delete("/document/delete/{id}", documentId))
+                .andExpect(status().isOk());
+
+        verify(documentServiceImpl, times(1)).deleteById(documentId);
+    }
+
+
+    @Test
+    public void DocumentController_DeleteById_ShouldReturnClientError_WhenExceptionThrown() throws Exception {
+        Integer documentId = 99;
+
+        // Kada deleteById baci exception
+        when(documentServiceImpl.deleteById(documentId))
+                .thenThrow(new RuntimeException("Error deleting"));
+
+        mockMvc.perform(delete("/document/delete/{id}", documentId))
+                .andExpect(status().is4xxClientError());
+
+        verify(documentServiceImpl, times(1)).deleteById(documentId);
     }
 
 }
